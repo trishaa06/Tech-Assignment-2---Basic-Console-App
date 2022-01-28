6ecd591e3f5cf859f68e6d92a8d6964b146fc459
trish <trish@TRISHA>
2022-01-27 22:57:11 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Name: Trisha Trisha //
+using System;
+
+namespace Tech_Assignment_2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It has become appallingly obvious that our technology has exceeded our humanity.");
+            Console.WriteLine("Submitted by Trisha Trisha.");
+        }
+    }
+}
